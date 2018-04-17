52788399d408c664bad86f7d4d4c13d9052afee0
paweleck <czemierysp@op.pl>
2018-04-17 16:38:15 +0200

    2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 var phonecatApp = angular.module('PKOCM', []);
 
 phonecatApp.controller('MainController', function PhoneListController($scope) {
+  const DESC_LIMIT = 100;
+  const NO_LIMIT = 999999;
+
+  $scope.switchLimit = function(operation) {
+    if (operation.descLimit == DESC_LIMIT) {
+      operation.descLimit = NO_LIMIT;
+    } else {
+      operation.descLimit = DESC_LIMIT;
+    }
+  };
+
+  $scope.ignoreChange = function(operation) {
+    if (operation.ignore) {
+      operation.descLimit = NO_LIMIT;
+    } else {
+      operation.descLimit = DESC_LIMIT;
+    }
+  };
+
     $scope.operations = [{
         "exec_date": "2018-04-16",
         "order_date": "2018-04-14",
@@ -8,6 +27,14 @@ phonecatApp.controller('MainController', function PhoneListController($scope) {
         "description": "Tytuł: 000483849 74838498104086481636291 Lokalizacja: Kraj: POLSKA Miasto: Bialystok Adres: Roxy Burgers Data i czas operacji: 2018-04-14 13:15:52 Oryginalna kwota operacji: 14,00 PLN Numer karty: 42512509*****343",
         "amount": -14.00,
         "ending_balance": 40136.67,
-        "visible": true
+        "ignore": false
+    },{
+        "exec_date": "2018-04-16",
+        "order_date": "2018-04-14",
+        "type": "Płatność kartą",
+        "description": "Tytuł: 000483849 74838498104086481636291 Lokalizacja: Kraj: POLSKA Miasto: Bialystok Adres: Roxy Burgers Data i czas operacji: 2018-04-14 13:15:52 Oryginalna kwota operacji: 14,00 PLN Numer karty: 42512509*****343",
+        "amount": -14.00,
+        "ending_balance": 40136.67,
+        "ignore": false
     }];
 });
---
+++ js/non_ang.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', onLoad, false );
+
+function onLoad() {
+  var oper_descs = document.getElementsByClassName("oper-desc");
+
+  for (var i = 0; i < oper_descs.length; i++) {
+    var oper_desc = oper_descs[i];
+    oper_desc.addEventListener('mousedown', function (e) {
+      e.preventDefault();
+    }, false);
+  }
+}
